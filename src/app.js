7b9de6bd6e83529071be5805ce141dd03e4b73ed
dangolbeeker <scottb305@outlook.com>
2019-07-15 20:16:36 -0400

    imported react component with state and render methods

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            items: []
+        };
+    }
+
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
